4a26323d1330f5c6213cca6cf6b143962c71721f
Isaac Suttell <isaac_suttell@playstation.sony.com>
2017-01-11 08:20:32 -0800

    Updated examples to include a long list

--- examples/index.jsx
+++ examples/index.jsx
@@ -9,13 +9,33 @@ import Select from '../src/Select';
 class Examples extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      manyOpts: this.getManyOptions()
+    };
   }
 
   handleChange(key, event) {
     let state = {};
     state[key] = event.target.value;
     this.setState(state);
+  }
+
+  getManyOptions(len = 50) {
+    let opts = [];
+    let index = -1
+    while(index++ < len) {
+      var value = '';
+      for (var i = 0; i < 5; i++) {
+        value += String.fromCharCode(Math.floor(Math.random() * 26) + 65).toLowerCase();
+      }
+
+      const opt = {
+        value: value,
+        label: value.charAt(0).toUpperCase() + value.substring(1)
+      };
+      opts.push(opt);
+    }
+    return opts;
   }
 
   render() {
@@ -41,18 +61,18 @@ class Examples extends React.Component {
         <div className='example-group'>
           <h2>Default</h2>
           <Select
-            defaultValue={this.state.example1 || opts.str[0]}
+            value={this.state.example1 || opts.str[0]}
             options={opts.str}
             icon={<span className='ion-chevron-down'/>}
             onChange={this.handleChange.bind(this, 'example1')} />
           <code>
             {`<Select
               onChange={this.handleChange}
-              defaultValue="One"
+              value="One"
               options={["One", "Two", "Three"]} />`}
           </code>
           <Select
-            defaultValue={this.state.example2 || opts.obj[0].value}
+            value={this.state.example2 || opts.obj[0].value}
             options={opts.obj}
             icon={<span className='ion-chevron-down'/>}
             onChange={this.handleChange.bind(this, 'example2')}
@@ -60,7 +80,7 @@ class Examples extends React.Component {
           <code>
             {`<Select
               onChange={this.handleChange}
-              defaultValue="1"
+              value="1"
               options={[
                   {
                     value: '1',
@@ -79,7 +99,7 @@ class Examples extends React.Component {
           <h2>With Label</h2>
           <Select
             label='Select...'
-            defaultValue={this.state.example3}
+            value={this.state.example3}
             options={opts.str}
             icon={<span className='ion-chevron-down'/>}
             onChange={this.handleChange.bind(this, 'example3')} />
@@ -94,7 +114,7 @@ class Examples extends React.Component {
           <h2>Disabled</h2>
           <Select
             disabled
-            defaultValue={this.state.example4 || opts.str[0]}
+            value={this.state.example4 || opts.str[0]}
             options={opts.str}
             icon={<span className='ion-chevron-down'/>}
             onChange={this.handleChange.bind(this, 'example4')} />
@@ -104,6 +124,16 @@ class Examples extends React.Component {
               onChange={this.handleChange}
               options={["One", "Two", "Three"]} />`}
           </code>
+        </div>
+        <div className='example-group'>
+          <h2>Long List</h2>
+          <Select
+            label='Select...'
+            value={this.state.example5}
+            options={this.state.manyOpts}
+            icon={<span className='ion-chevron-down'/>}
+            onChange={this.handleChange.bind(this, 'example5')}
+          />
         </div>
       </div>
     );
